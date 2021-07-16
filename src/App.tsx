83bafd735124e91994d7d5efca3787b242132441
Kyle <kylereaves@gmail.com>
2021-07-16 00:44:43 -0400

    added comments

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Form } from './Form/Form';
 import { Table } from './Table/Table';
 
 function App() {
+  // people doesn't warrant its own type, since 'people' are multiple Persons
   const [people, setPeople] = React.useState<Person[]>([])
 
   function addPerson(person: Person) {
@@ -14,7 +15,10 @@ function App() {
   return (
     <div className="App">
       <h1><code>Kyle's Genealogy Thing</code></h1>
+      {/* the 'people' state managed in App 
+      is passed to the Table to populate it */}
       <Table people={people} />
+      {/* a state setter is passed to the Form */}
       <Form onSave={addPerson} />
     </div>
   );
