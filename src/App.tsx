f4e2bd2794205d2faa04ec472e4fb190749498bc
Kyle <kylereaves@gmail.com>
2021-11-02 11:28:56 -0400

    Imported FormComponent, added BS css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
 import { Person } from './Person';
-import { Form } from './Form/Form';
+import { FormComponent } from './Form/Form';
 import { Table } from './Table/Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   // people doesn't warrant its own type, since 'people' are multiple Persons
@@ -11,16 +12,16 @@ function App() {
   function addPerson(person: Person) {
     setPeople([...people, person]);
   }
-  
+
   return (
     <div className="App">
       <h1><code>Kyle's Genealogy Thing</code></h1>
       {/* the 'people' state managed in App  is passed to the Table to populate it */}
       <Table people={people} />
       {/* a state setter is passed to the Form */}
-      <Form onSave={addPerson} />
+      <FormComponent onSave={addPerson} />
     </div>
-  );
+  )
 }
 
 export default App;
