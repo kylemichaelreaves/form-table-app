9c4f4a5abff80c0930b148eaf91f0148f06b8e8c
Kyle <kylereaves@gmail.com>
2021-11-02 17:22:29 -0400

    Fixed improperly indented return content.

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -35,81 +35,81 @@ export function FormComponent({ onSave }: Props) {
 
     return <>
         <Form>
-                <Row>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text>first</InputGroup.Text>
-                        <Form.Control
-                            value={person.firstName}
-                            onChange={e => setPerson({ ...person, firstName: e.target.value })}
-                            aria-label="firstName"
-                        />
-                        <InputGroup.Text>middle</InputGroup.Text>
-                        <Form.Control
-                            aria-label="middleName"
-                            value={person.middleName}
-                            onChange={e => setPerson({ ...person, middleName: e.target.value })}
-                        />
-                        <InputGroup.Text>last</InputGroup.Text>
-                        <Form.Control
-                            aria-label="lastName"
-                            value={person.lastName}
-                            onChange={e => setPerson({ ...person, lastName: e.target.value })}
-                        />
-                    </InputGroup>
-                </Row>
-                <Row>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text>year</InputGroup.Text>
-                        <Form.Control
-                            aria-label="birthYear"
-                            value={person.birthYear}
-                            onChange={e => setPerson({ ...person, birthYear: e.target.value})}
-                            />
-                        <InputGroup.Text>month</InputGroup.Text>
-                        <Form.Control
-                            aria-label="birthMonth"
-                            value={person.birthMonth}
-                            onChange={e => setPerson({ ...person, birthMonth: e.target.value})}
-                            />
-                        <InputGroup.Text>day</InputGroup.Text>
-                        <Form.Control
-                            aria-label="birthDay"
-                            value={person.birthDay}
-                            onChange={e => setPerson({ ...person, birthDay: e.target.value})} />
-                    </InputGroup>
-                </Row>
-                <Row>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text className="mb-3">state of birth</InputGroup.Text>
-                        <Form.Select
-                            className="mb-3"
-                            value={person.birthState}
-                            placeholder='state of birth'
-                            onChange={e => setPerson({ ...person, birthState: e.target.value })}>
-                            {states.map(state =>
-                                <option key={state} value={state}>
-                                    {state}
-                                </option>
-                            )}
-                        </Form.Select>
-                        <InputGroup.Text className="mb-3">county of birth</InputGroup.Text>
-                        <Form.Select
-                            className="mb-3"
-                            value={person.birthCounty}
-                            placeholder='county of birth'
-                            onChange={e => setPerson({ ...person, birthCounty: e.target.value })}>
-                            {getCounties(person.birthState)}
-                        </Form.Select>
-                        <InputGroup.Text className="mb-3">town or city of birth</InputGroup.Text>
-                        <Form.Control
-                            className="mb-3"
-                            value={person.birthTown}
-                            placeholder='town or city of birth'
-                            onChange={e => setPerson({ ...person, birthTown: e.target.value })}
-                        />
-                    </InputGroup>
-                </Row>
-            </Form>
+            <Row>
+                <InputGroup className="mb-3">
+                    <InputGroup.Text>first</InputGroup.Text>
+                    <Form.Control
+                        value={person.firstName}
+                        onChange={e => setPerson({ ...person, firstName: e.target.value })}
+                        aria-label="firstName"
+                    />
+                    <InputGroup.Text>middle</InputGroup.Text>
+                    <Form.Control
+                        aria-label="middleName"
+                        value={person.middleName}
+                        onChange={e => setPerson({ ...person, middleName: e.target.value })}
+                    />
+                    <InputGroup.Text>last</InputGroup.Text>
+                    <Form.Control
+                        aria-label="lastName"
+                        value={person.lastName}
+                        onChange={e => setPerson({ ...person, lastName: e.target.value })}
+                    />
+                </InputGroup>
+            </Row>
+            <Row>
+                <InputGroup className="mb-3">
+                    <InputGroup.Text>year</InputGroup.Text>
+                    <Form.Control
+                        aria-label="birthYear"
+                        value={person.birthYear}
+                        onChange={e => setPerson({ ...person, birthYear: e.target.value })}
+                    />
+                    <InputGroup.Text>month</InputGroup.Text>
+                    <Form.Control
+                        aria-label="birthMonth"
+                        value={person.birthMonth}
+                        onChange={e => setPerson({ ...person, birthMonth: e.target.value })}
+                    />
+                    <InputGroup.Text>day</InputGroup.Text>
+                    <Form.Control
+                        aria-label="birthDay"
+                        value={person.birthDay}
+                        onChange={e => setPerson({ ...person, birthDay: e.target.value })} />
+                </InputGroup>
+            </Row>
+            <Row>
+                <InputGroup className="mb-3">
+                    <InputGroup.Text className="mb-3">state of birth</InputGroup.Text>
+                    <Form.Select
+                        className="mb-3"
+                        value={person.birthState}
+                        placeholder='state of birth'
+                        onChange={e => setPerson({ ...person, birthState: e.target.value })}>
+                        {states.map(state =>
+                            <option key={state} value={state}>
+                                {state}
+                            </option>
+                        )}
+                    </Form.Select>
+                    <InputGroup.Text className="mb-3">county of birth</InputGroup.Text>
+                    <Form.Select
+                        className="mb-3"
+                        value={person.birthCounty}
+                        placeholder='county of birth'
+                        onChange={e => setPerson({ ...person, birthCounty: e.target.value })}>
+                        {getCounties(person.birthState)}
+                    </Form.Select>
+                    <InputGroup.Text className="mb-3">town or city of birth</InputGroup.Text>
+                    <Form.Control
+                        className="mb-3"
+                        value={person.birthTown}
+                        placeholder='town or city of birth'
+                        onChange={e => setPerson({ ...person, birthTown: e.target.value })}
+                    />
+                </InputGroup>
+            </Row>
+        </Form>
 
         {/* setting the person,
         incrementing the intial id to generate unique ids,
