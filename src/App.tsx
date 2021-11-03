00de58351045390fd1839ff00425b108ea14b3bf
Kyle Reaves <89040357+KyleReaves@users.noreply.github.com>
2021-11-03 14:40:08 -0400

    Table name changed to TableComponent
    to avoid conflict with the Bootstrap component.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { Person } from './Person';
 import { FormComponent } from './Form/Form';
-import { Table } from './Table/Table';
+import { TableComponent } from './Table/Table';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
     <div className="App">
       <h1><code>Kyle's Genealogy Thing</code></h1>
       {/* the 'people' state managed in App  is passed to the Table to populate it */}
-      <Table people={people} />
+      <TableComponent people={people} />
       {/* a state setter is passed to the Form */}
       <FormComponent onSave={addPerson} />
     </div>
